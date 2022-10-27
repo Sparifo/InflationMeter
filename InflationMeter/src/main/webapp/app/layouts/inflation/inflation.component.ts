a79e3deae8d00a4d31a7c4b235dd72da460bc5e6
Mateusz Miernik <mat.miernik@hotmail.com>
2022-10-28 00:06:47 +0200

    Added component inflation

---
+++ InflationMeter/src/main/webapp/app/layouts/inflation/inflation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-inflation',
+  templateUrl: './inflation.component.html',
+  styleUrls: ['./inflation.component.scss']
+})
+export class InflationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
